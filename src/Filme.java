5e559cdd10e30eab4066a3b0bf3bf6da6d9bdee9
DevGrotti <matheusgrottipereira@gmail.com>
2024-12-08 11:51:31 -0300

    Primeiro commit

---
+++ src/Filme.java
@@ -0,0 +1,24 @@
+public class Filme {
+    // abstraçao - uma classe guarda quais caracteristicas tem determinado objeto
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+}
